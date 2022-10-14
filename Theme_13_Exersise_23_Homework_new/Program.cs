f9ac01fdf9b4ca4b865fcc84f4d94251fe9ab74f
Andrew_A <chen_60@mail.ru>
2022-10-14 08:35:07 +0300

    Create and Added Theme_13_Exersise_23_Homework_new

---
+++ Theme_13_Exersise_23_Homework_new/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23 РЕШЕНА!
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine();
+static void ShowQube(int n)
+{
+    int count = 1;
+    while (count <= n)
+    {
+    Console.Write(Math.Pow(count, 3) + ", ");
+    count++;
+    }
+}
+Console.Write("Input your digit: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($" For n = {n} sequences is: ");
+ShowQube(n);
+Console.WriteLine();
+Console.WriteLine();
