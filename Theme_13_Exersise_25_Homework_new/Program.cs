5ef12e58553e606102aadbc6aac97432cb7f7581
Andrew_A <chen_60@mail.ru>
2022-10-17 04:24:56 +0300

    Created and Added  Theme_13_Exersise_25_Homework_new

---
+++ Theme_13_Exersise_25_Homework_new/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 25: РЕШЕНА
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+double GetExponent(double a, double b)
+{
+   double num = a; 
+   double count = 1;
+
+    while (count < b)
+   {
+        num = num * a;
+        count++;    
+   }
+    return num; 
+}
+
+Console.Write("Input your digit: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input degree of you need: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($" {GetExponent(a,b)} ");
